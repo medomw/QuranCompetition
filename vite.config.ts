ffca9b70cb533df0d2cfe8a61065bafbab406c13
medomw <mohamedgomaamedomedo@gmail.com>
2026-03-25 10:54:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/QuranCompetition/",
-
-  server: {
-    host: "::",
-    port: 8080,
-  },
-
-  plugins: [react()],
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
-  build: {
-    outDir: "dist",
-  },
+server: {
+host: "::",
+port: 8080,
+},
+plugins: [
+react(),
+],
+resolve: {
+alias: {
+"@": path.resolve(__dirname, "./src"),
+},
+},
 });
